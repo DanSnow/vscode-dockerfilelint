6a2a38f6db864c68d7c31c82526b0601c3fc46f9
DanSnow <dododavid006@gmail.com>
2019-04-02 21:00:07 +0800

    chore: Add eslint; Remove tslint

---
+++ .eslintrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['standard', 'plugin:@typescript-eslint/recommended', 'prettier/@typescript-eslint'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'prettier']
+}
