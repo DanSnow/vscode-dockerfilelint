45ea034e8564fdf651b100cac2b5154caabc8ab0
DanSnow <dododavid006@gmail.com>
2021-03-14 21:08:37 +0800

    test: fix test runner

--- scripts/run-test.ts
+++ scripts/run-test.ts
@@ -1,16 +1,15 @@
-import * as path from 'path'
-
+import { resolve } from 'path'
 import { runTests } from 'vscode-test'
 
 async function main() {
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
-    const extensionDevelopmentPath = path.resolve(__dirname, '../dist')
+    const extensionDevelopmentPath = resolve(__dirname, '../dist')
 
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, '../dist/test.js')
+    const extensionTestsPath = resolve(__dirname, '../out/test.js')
 
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath })
